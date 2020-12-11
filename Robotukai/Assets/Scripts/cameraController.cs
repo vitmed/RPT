97e46b8cb4c217d88087581d8b53a052a02b8d92
vainius21 <vainius1232@gmail.com>
2020-12-11 17:25:56 +0200

    Spectator
    Kamera sukiojasi su pele, vaiksto su wasd arba rodyklytem.

---
+++ Robotukai/Assets/Scripts/cameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public float moveSpeed = 1f;
+    public Transform cameraBody;
+    public CharacterController controller;
+    float xRotation = 0f;
+    float yRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotation
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        yRotation += mouseX;    
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+        //Movement (WASD||arrow keys)
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
